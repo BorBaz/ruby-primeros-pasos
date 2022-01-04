baa3a645ca391b253c8ae2a736a0b4584fd302db
Borja <boorjabs@gmail.com>
2022-01-04 23:52:44 +0100

    Functions with numbers

---
+++ src/numbers/functions_numbers.rb
@@ -0,0 +1,11 @@
+def escribe_numero(numero)
+  puts "Tu numero es: " + numero
+end
+
+def escribe_numero_y_suma_dos(numero)
+  puts "Tu numero es: "
+  puts numero + 2
+end
+
+escribe_numero("3")
+escribe_numero_y_suma_dos(1)
