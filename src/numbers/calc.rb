7e5eee7023895ce94340d6ec63f2278fee8971f1
Borja <33966820+borbaz@users.noreply.github.com>
2022-01-04 23:08:02 +0100

    Numbers manipulation in Ruby

---
+++ src/numbers/calc.rb
@@ -0,0 +1,34 @@
+# Puts saca por pantalla info
+# Comportamiento de los numeros
+# puts 1 + 2
+#
+# puts 1.0 + 2.0
+# puts 1.0 * 2.0
+# puts 6.0 - 9.0
+# puts 5.0 / 2.0
+#
+# puts 1 + 2
+# puts 1 * 2
+# puts 6 - 9
+# puts 5 / 2
+#
+# puts 5 * 9 - (54 / 2) - 55
+
+# ¿Cuántas horas hay en un año?
+puts "¿Cuántas horas hay en un año?"
+
+puts 365 * 24
+
+# ¿Cuántos minutos hay en una década?
+
+puts "¿Cuántos minutos hay en una década?"
+puts (365 * 10) * (24 * 60)
+
+#  ¿Cuántos segundos de edad tienes?
+puts "¿Cuántos segundos de edad tienes?"
+puts 26 * (365 * 24 * 3600)
+
+# ¿Cuántos chocolates esperas comer en tu vida?
+puts "¿Cuántos chocolates esperas comer en tu vida?"
+puts 2 * (24 * 52)
+
