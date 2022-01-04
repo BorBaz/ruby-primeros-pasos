a351f23f4318451e3a06c0c8ff91150c7948891c
Borja <boorjabs@gmail.com>
2022-01-05 00:43:41 +0100

    Formatting

--- src/numbers/calc.rb
+++ src/numbers/calc.rb
@@ -15,17 +15,17 @@
 # puts 5 * 9 - (54 / 2) - 55
 
 # ¿Cuántas horas hay en un año?
-puts "¿Cuántas horas hay en un año?"
+puts '¿Cuántas horas hay en un año?'
 puts 365 * 24
 
 # ¿Cuántos minutos hay en una década?
-puts "¿Cuántos minutos hay en una década?"
+puts '¿Cuántos minutos hay en una década?'
 puts (365 * 10) * (24 * 60)
 
 #  ¿Cuántos segundos de edad tienes?
-puts "¿Cuántos segundos de edad tienes?"
+puts '¿Cuántos segundos de edad tienes?'
 puts 26 * (365 * 24 * 3600)
 
 # ¿Cuántos chocolates esperas comer en tu vida?
-puts "¿Cuántos chocolates esperas comer en tu vida?"
+puts '¿Cuántos chocolates esperas comer en tu vida?'
 puts 2 * (24 * 52)
