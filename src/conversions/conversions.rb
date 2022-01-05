0b4db3ba7e24de5c461f85058e2babcea5db2506
Borja <boorjabs@gmail.com>
2022-01-05 02:21:09 +0100

    Introducing conversions

---
+++ src/conversions/conversions.rb
@@ -0,0 +1,25 @@
+#Vamos a ver como funcionan las conversiones
+
+#Convirtiendo a String
+number = 1
+this_string = 'Mi cadena '
+
+puts this_string + number.to_s
+
+# Conviertiendo a int
+number = 1
+this_string_as_number = '5'
+puts this_string_as_number.to_i + number
+
+# Mas conversiones
+puts '60'.to_f
+puts '99.999'.to_f
+puts '55.243'.to_i
+puts 'Que pasa si hay un 5 en un string'.to_i
+puts 'Y si hay 2 numeros 5'.to_i
+puts 'Y si no hay nada'.to_i
+puts 1.to_i
+
+
+# puts significa put string, y convierte su valor parametrizado siempre a una cadena de texto
+
