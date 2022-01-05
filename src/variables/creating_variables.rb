c72b0e12dd22f53e349b7a421fbc5e988684b422
Borja <boorjabs@gmail.com>
2022-01-05 02:03:17 +0100

    Introducing variables

---
+++ src/variables/creating_variables.rb
@@ -0,0 +1,29 @@
+# Vamos a crear algunas variables en Ruby
+
+my_string = "Hello World"
+puts  my_string
+
+name = "Borja"
+puts "Mi nombre es " + name
+name = "El Pepe"
+puts " ... Pero me  he cambiado de nombre, ahora soy " + name
+
+
+# A Ruby se la sopla el tipo de dato que sea la variable
+lechuga = 45
+puts lechuga
+lechuga = "Bad Bunny"
+puts lechuga
+
+
+# Y como funciona asignar variables unas a otras
+una_variable = "Una variable"
+otra_variable = "Otra variable"
+
+puts una_variable
+puts otra_variable
+puts "Reasignamos variables..."
+una_variable = otra_variable
+puts una_variable
+puts otra_variable
+
